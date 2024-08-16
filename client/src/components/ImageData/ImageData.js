875b7af7e1f660929370c6942490b8dd184f1949
Alestaubin <alexandre.st-aubin2@mail.mcgill.ca>
2024-08-16 21:06:44 +0200

    touch screen stuff

--- client/src/components/ImageData/ImageData.js
+++ client/src/components/ImageData/ImageData.js
@@ -1,8 +1,8 @@
 const UnsplashImages = [
     {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721916641/upk6c3gyblk7qbwnmtvc.jpg",
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721916886/portfolio/wgllaawqpt1hisr89jhk.jpg",
         title: "Grand Prix Vélocité",
-        description: null
+        description: "Final sprint of the Grand Prix Vélocité in Montréal."
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1723808744/personal/Norge/vqzt5f9dcj9us4jvyeur.jpg",
@@ -12,42 +12,42 @@ const UnsplashImages = [
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1723808747/personal/Norge/xahbhhkpe5vkhkoudpz4.jpg",
         title: "Lofoten",
-        description: null
+        description: "Aboard the Hurtigruten, a cruise ship that travels along the coast of Norway."
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721916909/portfolio/t7kl4lmixh4mdwqdlvme.jpg",
-        title: "Ocean",
-        description: null
+        title: "Cape Cod",
+        description: "Silouhettes of a boy and a fishing boat at sunset in Cape Cod."
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721917382/portfolio/sgmqafzqrhdhz23rdvcy.jpg",
         title: "CN",
-        description: null
+        description: "The CN Tower in Toronto."
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721917382/portfolio/ugxtkl6uhkpgqozsyjql.jpg",
         title: "Toronto",
-        description: null
+        description: "The Toronto skyline."
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721917382/portfolio/yebt4d5fwxetdldj31j8.jpg",
         title: "Yonge",
-        description: null
+        description: "Yonge Street in Toronto."
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721917406/portfolio/xwsnzqn4i4nsug7nohdi.jpg",
-        title: "Portraits HEC",
-        description: null
+        title: "HEC Montréal",
+        description: "Portrait of students at HEC Montréal."
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721917459/portfolio/hrb1bl3ofkazgfayry8y.jpg",
         title: "Mardis Cyclistes",
-        description: null
+        description: "Riders in the Mardis Cyclistes de Lachine."
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721917459/portfolio/evv954slky3w7crdva1z.jpg",
         title: "Mardis Cyclistes",
-        description: null
+        description: "Riders in the Mardis Cyclistes de Lachine."
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721916904/portfolio/zshcdizvevlycwzdgekl.jpg",
