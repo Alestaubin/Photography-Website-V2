33ffc3134d908820d6062cabf65721077dabd646
Alestaubin <alexandre.st-aubin2@mail.mcgill.ca>
2024-10-24 12:49:34 -0400

    commercial --> people

--- client/src/index.js
+++ client/src/index.js
@@ -57,7 +57,7 @@ ReactDOM.render(
         <Route path="/Toronto" element={<Toronto />} />
         <Route path="/Norge" element={<Norge />} />
         <Route path="/Golf" element={<Golf />} />
-      <Route path="/Commercial" element={<People />} />
+      <Route path="/Commercial" element={<Commercial />} />
         <Route path="/MARIAGE_RAPH_FEL_24" element={<MARIAGE_RAPH_FEL_24 />} />
         <Route path="/Hec5" element={<Hec5 />} />
         <Route path="/Hec24" element={<Hec24 />} />
