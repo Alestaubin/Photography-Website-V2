0ea2345f77a7e4e2b1c26c887076ed8088465fca
Alestaubin <alexandre.st-aubin2@mail.mcgill.ca>
2024-11-20 14:47:10 -0500

    added pixieset link in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,6 +6,7 @@ function Footer() {
       <footer className="d-flex flex-wrap justify-content-between align-items-center mx-5 px-5 fixed-bottom">
         <p className="col-md-4 mb-0 text-body-secondary">&copy; 2024 Alexandre St-Aubin</p>
         <ul className="nav col-md-4 justify-content-end">
+        <li className="nav-item"><a href="https://alexandrest-aubinphoto.pixieset.com" className="nav-link px-2 text-body-secondary"><img src="https://brandfetch.com/pixieset.com?view=library&library=default&collection=logos&asset=idXDOXmPZP" alt="Pixieset Logo"></img></a></li>
         <li className="nav-item"><a href="https://www.instagram.com/alex_staubin/" className="nav-link px-2 text-body-secondary"><img src="https://img.icons8.com/?size=50&id=32292&format=png&color=000000" alt="Instagram Logo"></img></a></li>
         </ul>
       </footer>
