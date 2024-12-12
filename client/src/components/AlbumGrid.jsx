bbe9f3eb09fdf8e488a837e6c16d56de9a4ec4c1
Alestaubin <alexandre.st-aubin2@mail.mcgill.ca>
2024-12-12 14:55:11 -0500

    margins

--- client/src/components/AlbumGrid.jsx
+++ client/src/components/AlbumGrid.jsx
@@ -32,7 +32,7 @@ function AlbumGrid({ items }) {
   }
 
   return (
-    <div id="image_display" className="container-fluid text-center overflow-auto p-4 py-5 mt-2 mb-3 mx-5">
+    <div id="image_display" className="container-fluid text-center overflow-auto p-4 py-5 mt-2 mb-3">
       <div className="album-grid" id="album-grid">
         <div className="album-grid-sizer"></div>
         {items.map((item, index) => (
