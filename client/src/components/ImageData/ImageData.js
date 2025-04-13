6922804f91016ebeb0fee8db7bcb538482529b78
Alestaubin <alexandre.st-aubin2@mail.mcgill.ca>
2025-04-13 15:47:02 -0400

    Modified the home page

--- client/src/components/ImageData/ImageData.js
+++ client/src/components/ImageData/ImageData.js
@@ -1,87 +1,319 @@
 const UnsplashImages = [
+
     {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721916886/portfolio/wgllaawqpt1hisr89jhk.jpg",
-        title: "Grand Prix Vélocité",
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570648/Portfolio2/hhvszxbfqatoujypyn5p.jpg",
+        title: null,
         description: null
     },
     {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1730693297/events/halloween-yoko/igx4udsvhw4eshwrebtz.jpg",
-        title: "Kiss",
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570655/Portfolio2/ijsxvogabttkebwzjcmj.jpg",
+        title: null,
         description: null
     },
     {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1730694597/events/halloween-rouge/mksjrxxi7ezqrznz415u.jpg",
-        title: "American Psycho",
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570651/Portfolio2/ub1srxxoozse3dmxahzv.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570649/Portfolio2/waxorl0k5gfiw4yr64yd.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570631/Portfolio2/deonfoncfegl6mnvbb6k.jpg",
+        title: null,
+        description: null
+    }, 
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570652/Portfolio2/h4iw5jtnldvbl2rw2rup.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570648/Portfolio2/gaqynwuxlqnohn5zxhdz.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570644/Portfolio2/sy4ytxapacn3ymrvbgoc.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570647/Portfolio2/ovyuq7i8ubymkyryatlv.jpg",
+        title: null,
+        description: null
+    },
+ 
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570638/Portfolio2/wechg1du6islv9vqvhzq.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570640/Portfolio2/rcfgwgox4bpjv7nlzqmi.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570642/Portfolio2/aeom1qn2gie1lqwu9lmz.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570655/Portfolio2/blne41hqzquahhis8y0h.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570636/Portfolio2/p8j2p6jfwwibdusaaiz4.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570635/Portfolio2/zo2l42tbvwldluox0a0f.jpg",
+        title: null,
+        description: null
+    },  
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570621/Portfolio2/ueploxji0erdqimll0tl.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570656/Portfolio2/t2kcyawdqcbv52bhdvhz.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570626/Portfolio2/qt6szvel83wwseaimrga.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570646/Portfolio2/prfjf93ffpghlyxogwk2.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570645/Portfolio2/qryawgnxf8wvpsjxextm.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570618/Portfolio2/swv9mag2ompkpfbodkdb.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570624/Portfolio2/y50yjwoxlruhqb6ahioj.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570630/Portfolio2/pjefsnpwnxvnbrcolksb.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570637/Portfolio2/rmoraajydrykttpf4711.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570633/Portfolio2/nflmtlr4cmcgczqovkgs.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570640/Portfolio2/preukwwlbxt2fx4mwutq.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570640/Portfolio2/td9hyiucpfybqwf7gxj9.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570630/Portfolio2/dzvrrwnrfxoxinhkkgid.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570639/Portfolio2/aydmnxyetmuzt6xe9jdg.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570620/Portfolio2/fqjlsjgngurrf6msax2w.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570626/Portfolio2/hlp55lmh0ygwn8zttlpo.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570628/Portfolio2/ep1kxr4wtdp2iivoejog.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403230/portfolio/rgydtbchezuiqtcm2viq.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/c_scale,w_960/v1730694615/events/halloween-rouge/daevtwcl73c2nrzrcyc0.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570644/Portfolio2/dgsk2cjr0zzphwqp7tno.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570627/Portfolio2/kuebi63ubxnmgthyzlku.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403238/portfolio/vkukcu5bircnpifm1sk4.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570653/Portfolio2/m5ck1wtlsqyeji3uwgia.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570654/Portfolio2/wttztofbsayqzvqqxbja.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570638/Portfolio2/bbwk69gkygrfj7jze2k4.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570654/Portfolio2/rcofd2x1ydfk7rnxzgvy.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744570655/Portfolio2/unvwmidtud0bwbve4zs2.jpg",
+        title: null,
+        description: null
+    },
+
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403257/portfolio/eiy2yrxfhqwmdyxwytfa.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403260/portfolio/plo4deu3xgc0ka1owvs0.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403256/portfolio/a7byrwsfdwaksjtohpec.jpg",
+        title: null,
+        description: null
+    },{
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403255/portfolio/yzn3jz8ajjrdubtflruj.jpg",
+        title: null,
+        description: null
+    },{
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403252/portfolio/radpew9kg8dxagjvx5xu.jpg",
+        title: null,
+        description: null
+    },{
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403251/portfolio/wptu7zxeq8q9ncvrejuv.jpg",
+        title: null,
+        description: null
+    },
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403228/portfolio/q8xfwtnybgxxqz4bubf8.jpg",
+        title: null,
+        description: null
+    },{
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403251/portfolio/ncx1dh7szzafkynlwi45.jpg",
+        title: null,
+        description: null
+    },{
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403249/portfolio/vfvrg9cphgf3amrl1au4.jpg",
+        title: null,
+        description: null
+    },{
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403248/portfolio/i6hgc54vumtbho39ip53.jpg",
+        title: null,
+        description: null
+    },{
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403248/portfolio/jw9gtv6fvxtbfa7jyrxf.jpg",
+        title: null,
         description: null
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1723808744/personal/Norge/vqzt5f9dcj9us4jvyeur.jpg",
-        title: "Aurlandsfjellet",
+        title: null,
         description: null
     },
     {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1730693309/events/halloween-yoko/s1w3luv3uynx1fgpwocx.jpg",
-        title: "DJ",
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403246/portfolio/mwjswisxhwjcinvaur8m.jpg",
+        title: null,
+        description: null
+    },{
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403245/portfolio/kxkkbg741izz6jtru3mc.jpg",
+        title: null,
+        description: null
+    },
+
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403229/portfolio/v9uyydyw0gmqnp2264ig.jpg",
+        title: null,
         description: null
     },
     {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1730245837/events/integ_uqam/errmsw2dzun2vpf1kwvc.jpg",
-        title: "UQAM ESG",
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403244/portfolio/vnlq242p1m29asdwaxfl.jpg",
+        title: null,
+        description: null
+    },
+
+    {
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403237/portfolio/tdckfpsjmvm1oe6ig3hb.jpg",
+        title: null,
         description: null
     },
     {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721916909/portfolio/t7kl4lmixh4mdwqdlvme.jpg",
-        title: "Cape Cod",
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403232/portfolio/vcn463bwu1uxgcnhce1f.jpg",
+        title: null,
         description: null
     },
     {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1728657694/sports/demiLachine2024/vnapl5dow09kve59qbln.jpg",
-        title: "Olympian Athlete",
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1744403227/portfolio/vo941tiyjuqzz9pmztgu.jpg",
+        title: null,
         description: null
     },
     {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1734023489/events/PERT-Mentorship/u2tcfbs6pumx5pt4xanx.jpg",
-        title: "Éric Girard",
-        description: null
-    },
-    {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1734397541/events/SSMU-GGG/rm6dz4z4ug19m5oedjvb.jpg",
-        title: "Singer",
-        description: null
-    },  
-    {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1730693297/events/halloween-yoko/oepsmhbxx3mus8yqectc.jpg",
-        title: "Watching from above",
-        description: null
-    },  
-    {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/c_scale,w_960/v1730694615/events/halloween-rouge/daevtwcl73c2nrzrcyc0.jpg",
-        title: "Real Clowns",
+        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721916886/portfolio/wgllaawqpt1hisr89jhk.jpg",
+        title: null,
         description: null
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721917459/portfolio/evv954slky3w7crdva1z.jpg",
-        title: "Mardis Cyclistes de Lachine",
-        description: null
-    },
-    {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1730693300/events/halloween-yoko/tddigfhdaiyy4erjapt9.jpg",
-        title: "Dancing Padme",
+        title: null,
         description: null
     },
     {
         src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1730692917/events/PERT-board/hhbwtqzhxc3eeizague0.jpg",
-        title: "PERT",
-        description: null
-    },
-    {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1734397536/events/SSMU-GGG/ypanmtjqoxpqpvncqo11.jpg",
-        title: "Killer",
-        description: null
-    },
-    {
-        src: "https://res.cloudinary.com/dkbw0dpyp/image/upload/v1721917406/portfolio/xwsnzqn4i4nsug7nohdi.jpg",
-        title: "HEC Montréal",
+        title: null,
         description: null
     }
 ];
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/react"
+//import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {
   Navigation,
